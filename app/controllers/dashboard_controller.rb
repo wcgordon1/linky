c7f2bd215dbbc34816cf14420169de9e2a01a2f8
wcgordon1 <wcgordon1@gmail.com>
2023-09-05 21:59:05 -0700

    routes and setting up profile page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,20 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_user, only: [:show]
   def index
     @should_render_navbar = true
+  end
+
+  
+  def show
+    redirect_to dashboard_path if @user.nil?
+  end
+    
+private 
+
+  def set_user
+    #localhost:3000/1
+    #localhost:3000/aphabetsoup
+    @user = User.find_by_id(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'dashboard', to: "dashboard#index"
   root 'dashboard#index'
+
+  #aollows us to use user_path(user) to get the URL for a user's profile page
+  get ':id', to: 'dashboard#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
